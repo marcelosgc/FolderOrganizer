7c860883f828820c19f1bedff18af8bdb0d8da74
Marcelo Carvallho <marcelo.sgcarvalho@gmail.com>
2024-02-21 18:06:08 +0000

    Added CustomException.cs and a return to menu if want to cancel a job

--- FolderOrganizer/Program.cs
+++ FolderOrganizer/Program.cs
@@ -10,48 +10,11 @@ var defaultFolder = "D:\\Downloads";
 
 StartMenu();
 
-do
-{
-    switch (option)
-    {
-        case 1:
-
-            OrganizeFolderByTypeJob.Execute(SetFolder());
-
-            break;
-        case 2:
-
-            OrganizeFolderByCreationDate.Execute(SetFolder());
-
-            break;
-        case 3:
-
-            PascalizeFoldersJob.Execute(SetFolder());
-            break;
-
-        case 4:
-
-            CleanDesktopJob.Execute();
-            break;
-
-        case 5:
-
-            MoveFolderContentJob.Execute(SetFolder());
-            break;
-    }
-
-    if (!IsExit(option))
-    {
-        WaitingState();
-    }
-} while (!IsExit(option));
-
-AnsiConsole.MarkupLine("Bye!");
+ExecuteOptionAndKeepAlive();
 
 bool IsExit(int i)
 {
     JobsEnum.TryParse("Exit", out JobsEnum value);
-
     return Convert.ToInt32(value) == i;
 }
 
@@ -59,7 +22,7 @@ void WaitingState()
 {
     AnsiConsole.MarkupLine("[grey69]--------- || ---------[/]");
     AnsiConsole.WriteLine();
-    AnsiConsole.MarkupLine("Ergh.. What do you want, again?");
+    AnsiConsole.MarkupLine("[springgreen3] Ergh.. What do you want, again? ¬_¬[/]");
     var sOption = AnsiConsole.Prompt(
         new SelectionPrompt<string>()
             .AddChoices(Enum.GetNames(typeof(JobsEnum)).ToList()));
@@ -80,10 +43,70 @@ void StartMenu()
     option = Convert.ToInt32(Enum.Parse<JobsEnum>(sOption));
 }
 
+void ExecuteOptionAndKeepAlive()
+{
+    do
+    {
+        try
+        {
+            switch (option)
+            {
+                case 1:
+
+                    OrganizeFolderByTypeJob.Execute(SetFolder());
+
+                    break;
+                case 2:
+
+                    OrganizeFolderByCreationDate.Execute(SetFolder());
+
+                    break;
+                case 3:
+
+                    PascalizeFoldersJob.Execute(SetFolder());
+                    break;
+
+                case 4:
+
+                    CleanDesktopJob.Execute();
+                    break;
+
+                case 5:
+
+                    MoveFolderContentJob.Execute(SetFolder());
+                    break;
+            }
+        }
+        catch (CustomException e)
+        {
+            AnsiConsole.MarkupLine("[bold red] 0 on directory input is to cancel the operation. Operation Canceled![/]");
+            AnsiConsole.WriteLine();
+        }
+        catch (Exception e)
+        {
+            AnsiConsole.WriteException(e);
+            AnsiConsole.WriteLine();
+        }
+
+        if (!IsExit(option))
+        {
+            WaitingState();
+        }
+    } while (!IsExit(option));
+    
+    AnsiConsole.WriteLine();
+    AnsiConsole.MarkupLine("Bye!");
+}
+
 string SetFolder()
 {
     string? newDir;
     newDir = AnsiConsole.Ask<string>("Set the folder to organize. Press enter to use the default folder");
+    if (newDir == "0")
+    {
+        throw new CustomException("0 as folder name to cancel the job");
+    }
+
     var chosenFolder = string.IsNullOrWhiteSpace(newDir) ? defaultFolder : newDir;
     return chosenFolder;
 }
